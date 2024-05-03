4b1cc37d3d32b542e13f1f3069aca131c9e6b47f
Jhon Alessandro <astralgmd@gmail.com>
2024-05-03 06:47:03 -0400

    refactor: separate pieces of advice for backend use and set method warning alternative if connection is not possible

--- assets/php/advice-fetching.php
+++ assets/php/advice-fetching.php
@@ -2,49 +2,10 @@
 
 declare(strict_types=1);
 
-# Creating the data for advice generator in an array
-//todo: Save all the pieces of advice in a different file, and make the array as a CONST variable
-$pieces_of_advice = [
-  [
-    "id" => 1,
-    "advice" => "It is easy to sit up and take notice, what's difficult is getting up and taking action."
-  ],
-  [
-    "id" => 2,
-    "advice" => "The best way to make a permanent change is to break the habit that's keeping you from it."
-  ],
-  [
-    "id" => 3,
-    "advice" => "The biggest risk is not taking any risk at all."
-  ],
-  [
-    "id" => 4,
-    "advice" => "The most important thing is to enjoy what you're doing."
-  ],
-  [
-    "id" => 5,
-    "advice" => "The opposite of courage is not weakness, it's fear."
-  ],
-  [
-    "id" => 6,
-    "advice" => "The only thing that interferes with our learning is our fear of not knowing."
-  ],
-  [
-    "id" => 7,
-    "advice" => "The only way to do great work is to love what you do."
-  ],
-  [
-    "id" => 8,
-    "advice" => "The only way to make progress is to take action."
-  ],
-  [
-    "id" => 9,
-    "advice" => "The only way to make things better is to change them."
-  ],
-];
+require_once "pieces-of-advice.php";
 
 # Getting the random advice after selection
-function get_random_advice(array $pieces_of_advice): array
+function get_random_advice(array $pieces_of_advice)
 {
   return $pieces_of_advice[array_rand($pieces_of_advice)];
 }
@@ -80,4 +41,7 @@ if ($_SERVER["REQUEST_METHOD"] === "GET") {
     echo return_advice_as_json($advice);
     exit;
   }
+} else {
+  header("HTTP/1.1 405 Method Not Allowed");
+  exit;
 }
---
+++ assets/php/pieces-of-advice.php
@@ -0,0 +1,45 @@
+<?php
+
+# Creating the data for advice generator in an array
+$pieces_of_advice = [
+  [
+    "id" => 1,
+    "advice" => "It is easy to sit up and take notice, what's difficult is getting up and taking action."
+  ],
+  [
+    "id" => 2,
+    "advice" => "The best way to make a permanent change is to break the habit that's keeping you from it."
+  ],
+  [
+    "id" => 3,
+    "advice" => "The biggest risk is not taking any risk at all."
+  ],
+  [
+    "id" => 4,
+    "advice" => "The most important thing is to enjoy what you're doing."
+  ],
+  [
+    "id" => 5,
+    "advice" => "The opposite of courage is not weakness, it's fear."
+  ],
+  [
+    "id" => 6,
+    "advice" => "The only thing that interferes with our learning is our fear of not knowing."
+  ],
+  [
+    "id" => 7,
+    "advice" => "The only way to do great work is to love what you do."
+  ],
+  [
+    "id" => 8,
+    "advice" => "The only way to make progress is to take action."
+  ],
+  [
+    "id" => 9,
+    "advice" => "The only way to make things better is to change them."
+  ],
+  [
+    "id" => 10,
+    "advice" => "What is worth to have it's worth to work for it."
+  ]
+];
