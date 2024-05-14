894488df54811ff2444f0758897426e2277c059c
Jhon Alessandro <astralgmd@gmail.com>
2024-05-13 21:33:55 -0400

    refactor: rename file for better comprehesion and update file requires

---
+++ assets/php/default-advice.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+# Creating the default data for advice generator in an array
+$pieces_of_advice = [
+  [
+    "id" => 1,
+    "advice" => "It is easy to sit up and take notice, what's difficult is getting up and taking action."
+  ],
+  [
+    "id" => 2,
+    "advice" => "The best way to make a permanent change is to break the habit that's keeping you from it."
+  ],
+  [
+    "id" => 3,
+    "advice" => "The biggest risk is not taking any risk at all."
+  ],
+  [
+    "id" => 4,
+    "advice" => "The most important thing is to enjoy what you're doing."
+  ],
+  [
+    "id" => 5,
+    "advice" => "The opposite of courage is not weakness, it's fear."
+  ],
+];
