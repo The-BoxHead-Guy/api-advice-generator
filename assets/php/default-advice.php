cee81cb2ec4e9ae74bc778aefa123d4235d3c981
Jhon Alessandro <astralgmd@gmail.com>
2024-05-15 19:14:44 -0400

    refactor(data): add new and more pieces of advice as static data

--- assets/php/default-advice.php
+++ assets/php/default-advice.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 # Creating the default data for advice generator in an array
-$pieces_of_advice = [
+$pieces_of_advices = [
   [
     "id" => 1,
     "advice" => "It is easy to sit up and take notice, what's difficult is getting up and taking action."
@@ -24,4 +24,64 @@ $pieces_of_advice = [
     "id" => 5,
     "advice" => "The opposite of courage is not weakness, it's fear."
   ],
+  [
+    "id" => 6,
+    "advice" => "The only limit to our realization of tomorrow will be our doubts of today."
+  ],
+  [
+    "id" => 7,
+    "advice" => "The future belongs to those who believe in the beauty of their dreams."
+  ],
+  [
+    "id" => 8,
+    "advice" => "The most difficult thing is the decision to act, the rest is merely tenacity."
+  ],
+  [
+    "id" => 9,
+    "advice" => "The only way to do great work is to love what you do."
+  ],
+  [
+    "id" => 10,
+    "advice" => "The key to success is taking massive, persistent action."
+  ],
+  [
+    "id" => 11,
+    "advice" => "The secret to getting ahead is getting started."
+  ],
+  [
+    "id" => 12,
+    "advice" => "The future belongs to those who believe in the beauty of their dreams."
+  ],
+  [
+    "id" => 13,
+    "advice" => "The most important thing is to enjoy what you're doing."
+  ],
+  [
+    "id" => 14,
+    "advice" => "The key to success is taking massive, persistent action."
+  ],
+  [
+    "id" => 15,
+    "advice" => "The secret to getting ahead is getting started."
+  ],
+  [
+    "id" => 16,
+    "advice" => "The future belongs to those who believe in the beauty of their dreams."
+  ],
+  [
+    "id" => 17,
+    "advice" => "The most important thing is to enjoy what you're doing."
+  ],
+  [
+    "id" => 18,
+    "advice" => "The key to success is taking massive, persistent action."
+  ],
+  [
+    "id" => 19,
+    "advice" => "The secret to getting ahead is getting started."
+  ],
+  [
+    "id" => 20,
+    "advice" => "The future belongs to those who believe in the beauty of their dreams."
+  ]
 ];
