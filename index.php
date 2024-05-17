a0dd63b87786029575c421b40c53fe28f815751d
Jhon Alessandro <astralgmd@gmail.com>
2024-05-16 21:05:40 -0400

    refactor(index): delete unnecesary code remaining

--- index.php
+++ index.php
@@ -4,20 +4,4 @@ declare(strict_types=1);
 
 // require_once __DIR__ . "/assets/php/db-insertion.php"; // Importing the database insertion PHP file
 require_once "assets/php/advice-fetching.php"; // Importing the advice fetching PHP file
-require_once "assets/php/functions.php"; // Importing the functions PHP file
 
-# Setting the advice for default use
-$id = set_id($advice);
-$advice = set_text($advice);
-
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<?php render_template("head"); // Renders the head template
-?>
-<?php render_template("body", ["id" => $id, "advice" => $advice]); // Renders the body template 
-?>
-
-</html>
