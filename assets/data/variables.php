4587627301284b3371ad1869fdb7fe5e8fbdb955
Jhon Alessandro <astralgmd@gmail.com>
2024-05-22 10:49:39 -0400

    chore(deleted-files): delete files that are not going to be used

--- assets/data/variables.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-# Making the connection with the DataBase "pieces_of_advices"
-const DSN = "mysql:host=localhost;dbname=pieces_of_advices";
-const USERNAME = "root";
-const PASSWORD = "Onix-DB";
