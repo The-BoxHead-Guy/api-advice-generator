002325383504f36229b07d6df141286b634f5b2a
Jhon Alessandro <astralgmd@gmail.com>
2024-05-29 16:50:43 -0400

    refactor(cronjob-class): delete redundant code and set new parameters for 'CreateLog' function

--- assets/classes/Cron/CreateLog.class.php
+++ assets/classes/Cron/CreateLog.class.php
@@ -10,30 +10,15 @@ namespace Cron;
 
 class CreateLog
 {
-  private static $inserted_data = 0;
-  # Gets the current date and time when the script it's executed
-
   private static $path = "/../../cronjobs/db-scan.log";
   private static $date_format = "F j, Y, g:i a";
 
   /**
-   * * Gets inserted data as INT in order to set $this->inserted_data
-   * 
-   * @param int 
-   * @return void
-   */
-  public static function set_inserted_data(int $amount): void
-  {
-    self::$inserted_data = $amount;
-  }
-
-  /**
    * * Creates log file with the current date and time and the status of the script
    *
-   * @param int
    * @return void
    */
-  public static function log_cronjob(): void
+  public static function log_cronjob(bool $status, string $textData): void
   {
     # Sets Venezuela 🇻🇪 timezone
     date_default_timezone_set("Etc/GMT+4");
@@ -44,46 +29,22 @@ class CreateLog
     $log = fopen(__DIR__ . self::$path, "a");
 
     # Writes the status in the log after succesful data insertion
-    if (self::$inserted_data > 0) {
+    if ($status) {
       fwrite(
         $log,
-        "Script executed at: " . $today . "\n" . "Data inserted: " . self::$inserted_data . "\n\n"
+        "Script executed at: " . $today . "\n" . "Data inserted: \"" . $textData . "\"" . "\n\n"
       );
     } else {
       fwrite(
         $log,
         "Script executed at: " . $today .
           "\n" .
-          "No data inserted, please add more information to be inserted into the database" . "\n\n"
+          "Repetition of advice caught: \"" . $textData . "\" already exists in DB." . "\n\n"
       );
     }
 
     fclose($log);
 
     echo "Log created successfully" . PHP_EOL;
-  }
-
-  /**
-   * * Notify which file has to be deleted due to its repetition
-   * 
-   * @param string
-   * @return void
-   */
-  public static function notify_advice_repetition(int $advice_id, string $advice)
-  {
-    $today = date(self::$date_format);
-
-    # Opens the log file
-    $log = fopen(__DIR__ . self::$path, "a");
-
-    # Writes the status in the log after succesful data insertion
-    fwrite(
-      $log,
-      "\nScript executed at: " . $today . "\n" . "Id: " . $advice_id . " whose text is: \"" . $advice . "\" has to be deleted. Cause: repetition" . "\n\n"
-    );
-
-    fclose($log);
-
-    echo "Error Log created successfully" . PHP_EOL;
   }
 }
