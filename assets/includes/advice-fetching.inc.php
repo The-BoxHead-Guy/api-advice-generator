da3ab1cc509006288035595ee4a92e732f8ba453
Jhon Alessandro <astralgmd@gmail.com>
2024-06-08 06:27:16 -0400

    chore(gitignore): set correct files to be hidden and install dependencies

--- assets/includes/advice-fetching.inc.php
+++ assets/includes/advice-fetching.inc.php
@@ -16,5 +16,16 @@ $piecesOfAdvices = $fetchAdvices->get_pieces_of_advices();
 # Getting the random advice after selection
 $advice = $fetchAdvices->get_random_advice($piecesOfAdvices);
 
+//todo: Use JWT authentication for: Registered users, Admins, and Guests users, and apply different app management for each of them.
+# Getting HTTP headers
+$headersRequests = apache_request_headers();
+
 # Handling the GET request for the advice
+/* switch (true) {
+  case !isset($headersRequests["Authorization"]) && $requestsMade > 10:
+    header("Authorization: null");
+    header("Reason: No Authorization header provided");
+    http_response_code(401);
+    break;
+} */
 JSONhandler::display_api_data($_SERVER['REQUEST_METHOD'], $advice);
